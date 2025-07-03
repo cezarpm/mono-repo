2196bd85992a53c5f58a9189c0c60e1bae5949ec
Cezar Patrizzi <cezar_pm@hotmail.com>
2025-07-03 14:10:28 -0300

    feat: more books

--- apps/test-api/src/index.ts
+++ apps/test-api/src/index.ts
@@ -17,6 +17,9 @@ const books: Book[] = [
   { id: 1, title: "O Hobbit", author: "J.R.R. Tolkien" },
   { id: 2, title: "1984", author: "George Orwell" },
   { id: 3, title: "Código Limpo", author: "Robert C. Martin" },
+  { id: 4, title: "Dom Casmurro", author: "Machado de Assis" },
+  { id: 5, title: "O Senhor dos Anéis", author: "J.R.R. Tolkien" },
+  { id: 6, title: "A Revolução dos Bichos", author: "George Orwell" },
 ];
 
 app.get("/books", (req: Request, res: Response) => {
